fd07d6d8972e0b47fe94a1c18952ea0affb007f9
Ramisetty <siva.ramisetty@capgemini.com>
2025-08-20 12:46:46 +0200

    Variables

---
+++ Variables.java
@@ -0,0 +1,25 @@
+public class Variables {
+
+    public static void main(String[] args){
+
+        int noOfLikes = 680;
+        int noOfDislikes = 50;
+
+        float price = 250.5f;
+
+        char sigleChar = 'D';
+
+        String tittle = "java springboot";
+
+        boolean isLiked = true;
+        boolean isPurchased = false;
+
+        System.out.println(noOfLikes);
+        System.out.println(isLiked);
+
+
+
+    }
+
+    
+}
