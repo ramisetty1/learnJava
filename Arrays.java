d322fbe415a7530706822da2f3e15fade1e7c7fc
siva <siva.ramisetty@capgemini.com>
2025-09-04 14:57:40 +0200

    Arrays

---
+++ Arrays.java
@@ -0,0 +1,24 @@
+class Arrays {
+
+    public static void main(String[] args){
+
+
+        String[] tittles = { "iphone 15", "pixel", "samsung", "honor" };
+
+        System.out.println(tittles[0]);
+
+        tittles[0] = "iphone 15 pro max";
+        System.out.println(tittles[0]);
+        System.out.println(tittles.length);
+
+        int[] prices = { 450, 5000, 852, 798 };
+
+        System.out.println(prices[2]);
+
+        System.out.println(prices.length);
+
+
+
+
+    }
+}
