e21d166f9834054a9f6f4785628f34efdd07ae75
siva <siva.ramisetty@capgemini.com>
2025-09-04 12:57:46 +0200

    switch

---
+++ SwitchCase.java
@@ -0,0 +1,28 @@
+public class SwitchCase {
+
+    public static void main(String[] args) {
+
+        String favViedosWatch = "Music";
+
+        switch(favViedosWatch){
+
+            case "Movies":
+                System.out.println("recomend movies releated");
+                break;
+            
+            case "Devotinal":
+                System.out.println("recomend Devotinal releated");
+                break;
+            case "study":
+                System.out.println("recomend study releated");
+                break;
+            case "Music":
+                System.out.println("recomend Music releated");
+                break;
+
+        
+        }
+        
+    }
+    
+}
